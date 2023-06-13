aa8cca080110aa53ac9abd7bef50c4d7eaab8b2f
Siim Bobkov <siim@warren.io>
2023-06-13 22:33:22 +0300

    react router Outlet for child components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Home from "./routes/home/home.component";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am Navigation bar</h1>
+      </div>
+      {/* Outlet is for rendering child components in subroutes */}
+      <Outlet />
+    </div>
+  );
+};
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+      </Route>
     </Routes>
   );
 };
