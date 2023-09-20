95c9e1dc3fc02f551057556d16a11e9561db8fc8
Siim Bobkov <siim@warren.io>
2023-09-21 00:16:00 +0300

    134. Category preview component and nested routes in Shop

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,12 @@
-import { Fragment, useContext } from "react";
-import { CategoriesContext } from "../../context/categories.context";
+import {Route, Routes} from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 import "./shop.styles.scss";
-import ProductCard from "../../components/product-card/product-card.component";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-  return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
-  );
-};
+return(
+  <Routes>
+    <Route index element={<CategoriesPreview />} />
+  </Routes>
+)};
 
 export default Shop;
