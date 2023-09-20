130f4f4ced49c07f51561d50dba929fe77864521
Siim Bobkov <siim@warren.io>
2023-09-20 23:35:58 +0300

    130. Get Products + Categories From Firestore

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,15 +1,18 @@
 import { createContext, useEffect, useState } from "react";
-import SHOP_DATA from "../shop-data";
-import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
   products: [],
 });
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-  // we use this useEffect to fire only once, when the component mounts
   useEffect(() => {
-    addCollectionAndDocuments("categories", SHOP_DATA);
+    // this is how you should use async/await in useEffect
+    const categoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    categoriesMap();
   }, []);
   const value = { products };
 
