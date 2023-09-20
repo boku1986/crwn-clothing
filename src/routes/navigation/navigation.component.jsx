81999eea92c7855dfb41c823594bcac6e64e685a
Siim Bobkov <siim@warren.io>
2023-09-20 20:17:28 +0300

    117. Toggle Cart Open

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,10 +6,11 @@ import { UserContext } from "../../context/user.context";
 import { signOutAuthUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../context/cart.context";
 
 const Navigation = () => {
-  const { currentUser} = useContext(UserContext);
-
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <>
@@ -30,9 +31,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
-          <CartIcon/>
+          <CartIcon />
         </div>
-        <CartDropdown/>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </>
