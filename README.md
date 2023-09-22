e70021efd1e28febe03623fa8c5d2768d8a3a751
Siim Bobkov <siim@warren.io>
2023-09-22 23:15:47 +0300

    153. React-Redux: Installation
    * start removing context API

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 #yarn add react-router-dom@6
 yarn add firebase
 ```
+## Add dependencies
+
+```bash
+
+bun add redux react-redux redux-logger
+```
---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,27 @@
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+//we need to give the state a default value (we dont have that hook anymore), so we set it to INITIAL_STATE
+// now all of our reducers react to every single action that gets fired, so we need to make sure
+// that we only update the state if the action.type matches the action type that we are looking for
+export const userReducer = (state = INITIAL_STATE, action) => {
+  // this is the reducer function, it takes in the current state and an action. Action has a type and a payload
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        // this way we just update the relevant part of the state
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      // if the action.type does not match any of the cases, we just return the state, to show that nothing has changed
+      return state;
+  }
+};
