8fd23b0064215e7a42cf35e78a72b9e7aec56629
Siim Bobkov <siim@warren.io>
2023-09-22 23:23:41 +0300

    154. React-Redux: Setting Up Our Store
    * start removing context API

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,10 @@
-import {compose,createStore,applyMiddleware} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import {logger} from "redux-logger/src";
+import {rootReducer} from "./root-reducer";
 
+//middleware
+const middlewares = [logger];
+//this is composition of middlewares (composition pattern)
+const composedEnhancers = compose(applyMiddleware(...middlewares));
 // root-reducer
-
+export const store = createStore(rootReducer, undefined, composedEnhancers);
