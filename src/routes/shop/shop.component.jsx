6f179f174321c52e0babf97ab679f8d513056e7e
Siim Bobkov <siim@warren.io>
2023-09-23 00:40:32 +0300

    157. move getting categories map to corresponding location
    * only components using this are Category and CategoriesPreview, so it should be in location where is their parent

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,8 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
+import { useEffect } from "react";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { setCategoriesMap } from "../../store/category/category.action";
+import { useDispatch } from "react-redux";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // this is how you should use async/await in useEffect
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+    getCategoriesMap();
+  }, [dispatch]);
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
