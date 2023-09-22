896a8c6b9c53b64696bf9fdfcb02cb85097f1561
Siim Bobkov <siim@warren.io>
2023-09-22 22:54:16 +0300

    fix console warning about styled-components props drilling
    * https://styled-components.com/docs/api#transient-props

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -11,7 +11,7 @@ const DirectoryItem = ({ category }) => {
   const onNavigateHandler = () => navigate(route)
   return (
     <DirectoryItemContainer onClick={onNavigateHandler}>
-      <BackgroundImage imageUrl={imageUrl} />
+      <BackgroundImage $imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
         <p>Shop now</p>
