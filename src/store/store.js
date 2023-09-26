8cbf93fe8ee92c3c5bbc0c61caecdea6cdd8d6d7
Siim Bobkov <siim@warren.io>
2023-09-26 14:22:52 +0300

    169. Redux-Persist - persist data on session and on reload

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,26 @@
-import {applyMiddleware, compose, createStore} from "redux";
-import {logger} from "redux-logger/src";
-import {rootReducer} from "./root-reducer";
+import { applyMiddleware, compose, createStore } from "redux";
+import { logger } from "redux-logger/src";
+import { rootReducer } from "./root-reducer";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 //middleware
 const middlewares = [logger];
 //this is composition of middlewares (composition pattern)
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 // root-reducer
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
