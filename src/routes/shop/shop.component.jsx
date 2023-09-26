6a9cda83ca1b8ac31078b4f0b6e8e81f1b008b73
Siim Bobkov <siim@warren.io>
2023-09-26 15:42:45 +0300

    172. Asynchronous Redux: Redux-Thunk
    * async behaviour of fetching and handling is moved to redux now with the thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,19 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { useEffect } from "react";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/category/category.action";
+import { fetchCategoriesAsync } from "../../store/category/category.action";
 import { useDispatch } from "react-redux";
 
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    // this is how you should use async/await in useEffect
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    };
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, [dispatch]);
   return (
     <Routes>
--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -1,5 +1,23 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORY_ACTION_TYPES } from "./category.types";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORY_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+
+export const fetchCategoriesFailure = (error) =>
+  createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_FAILURE, error);
+
+// this is our async thunk
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoriesList = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSuccess(categoriesList));
+  } catch (e) {
+    dispatch(fetchCategoriesFailure(e));
+  }
+};
