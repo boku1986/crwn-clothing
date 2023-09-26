41a0823f37b7f11ba562c00ef8fcbb42755e0aef
Siim Bobkov <siim@warren.io>
2023-09-26 20:31:50 +0300

    185. moving userReducer to redux-toolkit

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 import { SignUpContainer } from "./sign-up-form.styles";
-import { useDispatch } from "react-redux";
-import { signUpStart } from "../../store/user/user.action";
 
 const defaultFormFields = {
   displayName: "",
@@ -12,7 +14,6 @@ const defaultFormFields = {
   confirmPassword: "",
 };
 const SignUpForm = () => {
-  const dispatch = useDispatch();
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
@@ -27,7 +28,11 @@ const SignUpForm = () => {
       return;
     }
     try {
-      dispatch(signUpStart(email, password, displayName));
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
