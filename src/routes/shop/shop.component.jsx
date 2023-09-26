f4751f046329158a51ce5cb5b67d1e98ac91bde9
Siim Bobkov <siim@warren.io>
2023-09-26 20:32:01 +0300

    185. moving categories reducer to redux-toolkit

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,13 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { useEffect } from "react";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { useDispatch } from "react-redux";
-import {fetchCategoriesStart} from "../../store/category/category.action";
+import { setCategories } from "../../store/category/category.reducer";
 
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchCategoriesStart());
+    // this is how you should use async/await in useEffect
+    const getCategoriesMap = async () => {
+      const categoriesArray = await getCategoriesAndDocuments();
+      dispatch(setCategories(categoriesArray));
+    };
+    getCategoriesMap();
   }, [dispatch]);
   return (
     <Routes>
--- src/store/category/category.reducer.js
+++ src/store/category/category.reducer.js
@@ -1,31 +1,20 @@
-import { CATEGORY_ACTION_TYPES } from "./category.types";
+import { createSlice } from "@reduxjs/toolkit";
 
 const INITIAL_STATE = {
   categories: [],
   isLoading: false, // track whether we are fetching data from the server, for thunk
   error: null, // track any error that might occur, for thunk
 };
-export const categoryReducer = (state = INITIAL_STATE, action = {}) => {
-  const { type, payload } = action;
-  switch (type) {
-    case CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return {
-        ...state,
-        isLoading: true,
-      };
-    case CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return {
-        ...state,
-        categories: payload,
-        isLoading: false,
-      };
-    case CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_FAILURE:
-      return {
-        ...state,
-        error: payload,
-        isLoading: false,
-      };
-    default:
-      return state;
-  }
-};
+
+const categorySlice = createSlice({
+  name: "category",
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCategories: (state, action) => {
+      state.categories = action.payload;
+    },
+  },
+});
+
+export const { setCategories } = categorySlice.actions;
+export const categoryReducer = categorySlice.reducer;
