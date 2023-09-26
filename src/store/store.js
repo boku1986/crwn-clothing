010cf12d52e187763f4abf1ed6fa5ca56c217839
Siim Bobkov <siim@warren.io>
2023-09-26 19:27:37 +0300

    184. redux-toolkit replacing user-reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,43 +1,48 @@
-import { applyMiddleware, compose, createStore } from "redux";
-import { logger } from "redux-logger/src";
-import { rootReducer } from "./root-reducer";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
-import createSagaMiddleware from "redux-saga";
-import { rootSaga } from "./root-saga";
+// import { applyMiddleware, compose, createStore } from "redux";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["cart"],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-const sagaMiddleware = createSagaMiddleware();
-
-//middleware
-const middlewares = [
-  process.env.NODE_ENV !== "production" && logger,
-  sagaMiddleware,
-].filter(
+import {logger} from "redux-logger/src";
+import {configureStore} from "@reduxjs/toolkit";
+import {rootReducer} from "./root-reducer";
+// import { rootReducer } from "./root-reducer";
+// import storage from "redux-persist/lib/storage";
+// import { persistReducer, persistStore } from "redux-persist";
+// import createSagaMiddleware from "redux-saga";
+// import { rootSaga } from "./root-saga";
+//
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["cart"],
+// };
+//
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const sagaMiddleware = createSagaMiddleware();
+//
+// //middleware
+const middlewares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean // this way we filter out anything that is not true
 );
+//
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== "production" &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
+//
+// //this is composition of middlewares (composition pattern)
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+// // root-reducer
+// export const store = createStore(
+//   persistedReducer,
+//   undefined,
+//   composedEnhancers
+// );
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-
-//this is composition of middlewares (composition pattern)
-const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
-// root-reducer
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
-
-sagaMiddleware.run(rootSaga);
-
-export const persistor = persistStore(store);
+export const store = configureStore({
+  reducer: rootReducer,
+  // middleware: middlewares,
+});
+//
+// sagaMiddleware.run(rootSaga);
+//
+// export const persistor = persistStore(store);
